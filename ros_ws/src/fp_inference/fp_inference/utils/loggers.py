075d268e89f9b977c70917fd1e2b6658a733f228
AntoineRichard <antoine.richard@uni.lu>
2025-02-13 21:53:08 +0100

    added dummy save function

--- ros_ws/src/fp_inference/fp_inference/utils/loggers.py
+++ ros_ws/src/fp_inference/fp_inference/utils/loggers.py
@@ -1,20 +1,28 @@
+import datetime
+import os
+
 class Logger:
-    def __init__(self, logs_names:list=[], hooks: list = [], enabled: bool = False):
+    def __init__(self, logs_names:list=[], hooks: list = [], enabled: bool = False, save_path: str | None = None):
         self.logs_names = logs_names
         self.hooks = hooks
+
         self.enabled = enabled
+
+        if save_path is None:
+            raise ValueError("Save path is not defined.")
+        self.save_path = save_path
 
         self.build_logs()
 
     @property
-    def logs(self):
+    def logs(self) -> dict[str, list]:
         return self._logs
     
     @property
-    def logs_names(self):
+    def logs_names(self) -> list[str]:
         return self._logs.keys()
 
-    def build_logs(self):
+    def build_logs(self) -> None:
         self._logs = {}
         for logs_names in self.logs_names:
             for log_name in logs_names:
@@ -24,12 +32,27 @@ class Logger:
                 # Add the log name to the logs
                 self._logs[log_name] = []
 
-    def collect_logs(self, logs: dict):
+    def collect_logs(self, logs: dict) -> None:
+        """Collect logs from the hooks. The logs contain torch.Tensors.
+        
+        Args:
+            logs (dict): The logs to be collected."""
+
         for hook in self.hooks:
             logs = hook()
             for key, value in logs.items():
                 self._logs[key].append(value)
 
-    def update(self):
+    def update(self) -> None:
         if self.enabled:
             self.collect_logs()
+
+    def save(self, robot_interface_name: str, inference_runner_name: str, observation_formater_name: str) -> None:
+        if self.enabled:
+            date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            name = f"{inference_runner_name}_{robot_interface_name}_{observation_formater_name}_{date}"
+            os.makedirs(self.save_path, exist_ok=True)
+            os.makedirs(os.path.join(self.save_path, "logs"), exist_ok=True)
+
+            raise NotImplementedError("Save method not implemented.")
+
