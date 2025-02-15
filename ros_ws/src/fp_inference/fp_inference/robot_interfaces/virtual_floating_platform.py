029ff889ef27d9d14c3f8127870e541d795f39f0
AntoineRichard <antoine.richard@gatech.edu>
2025-02-15 12:25:07 +0100

    working commlink with virtual FP

--- ros_ws/src/fp_inference/fp_inference/robot_interfaces/virtual_floating_platform.py
+++ ros_ws/src/fp_inference/fp_inference/robot_interfaces/virtual_floating_platform.py
@@ -1,7 +1,7 @@
 from .base_robot_interface import BaseRobotInterface
 from . import Registerable
 
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import ByteMultiArray
 from gymnasium import spaces
 import torch
 import copy
@@ -9,29 +9,33 @@ import copy
 
 class VirtualFloatingPlatformInterface(Registerable, BaseRobotInterface):
     def __init__(self, *args, device: str | None = None, **kwargs):
-        super().__init__(*args, device=device**kwargs)
-        self._last_actions = torch.zeros((1, 8), device=self._device)
-        self.commands = Int16MultiArray()
-        self.commands.data = [0] * 9  # Everything off
+        super().__init__(*args, device=device, **kwargs)
 
-        self.ROS_ACTION_TYPE = Int16MultiArray
+        # Type of ROS message
+        self.ROS_ACTION_TYPE = ByteMultiArray
         self.ROS_ACTION_QUEUE_SIZE = 1
+
+        # Last actions is set to 0
+        self._last_actions = torch.zeros((1, 8), device=self._device)
+        self.commands = ByteMultiArray()
+        actions = [0] * 9  # Everything off
+        self.commands.data = [value.to_bytes(1, byteorder="little") for value in actions]
 
         # Action space
         self._action_space = spaces.MultiDiscrete([2] * 8)
 
     @property
-    def kill_action(self) -> Int16MultiArray:
-        kill_command = Int16MultiArray()
+    def kill_action(self) -> ByteMultiArray:
+        kill_command = ByteMultiArray()
         actions = [0] * 9
-        kill_command.data = [value.to_bytes(1, byteorder="little") for value in actions.int().tolist()]
+        kill_command.data = [value.to_bytes(1, byteorder="little") for value in actions]
         return kill_command
 
     def build_logs(self):
         super().build_logs()
         self._logs_specs["actions"] = [".t0", ".t1", ".t2", ".t3", ".t4", ".t5", ".t6", ".t7"]
 
-    def cast_actions(self, actions) -> Int16MultiArray:
+    def cast_actions(self, actions) -> ByteMultiArray:
         # Actions are expected to be either 0 or 1
         super().cast_actions(actions)
         # Ensure actions are between 0 and 1
@@ -39,7 +43,8 @@ class VirtualFloatingPlatformInterface(Registerable, BaseRobotInterface):
         # Store the actions
         self._last_actions = copy.copy(actions)
         # Convert the actions to bytes message
-        self.commands.data = [1] + actions.int().tolist()
+        actions = [1] + actions[0].int().tolist()
+        self.commands.data = [value.to_bytes(1, byteorder="little") for value in actions]
         # Return the commands
         return self.commands
 
@@ -47,5 +52,5 @@ class VirtualFloatingPlatformInterface(Registerable, BaseRobotInterface):
         super().reset()
         self._last_actions = torch.zeros((1, 8), device=self._device)
         actions = [0] * 9
-        self.commands.data = [value.to_bytes(1, byteorder="little") for value in actions.int().tolist()]
+        self.commands.data =[value.to_bytes(1, byteorder="little") for value in actions]
         self.build_logs()
