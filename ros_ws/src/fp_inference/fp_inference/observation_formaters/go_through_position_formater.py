edb519f464267ea5dafc44271c27c8f6b4720c58
Luis Felipe Wolf Batista <luis.batista@gatech.edu>
2025-02-21 18:32:58 +0100

    Added Luis base GoThroughPose

---
+++ ros_ws/src/fp_inference/fp_inference/observation_formaters/go_through_position_formater.py
@@ -0,0 +1,114 @@
+from . import Registerable
+from . import BaseFormater, BaseFormaterCfg
+from fp_inference.state_preprocessors import BaseStatePreProcessor
+
+from geometry_msgs.msg import PointStamped
+from dataclasses import dataclass
+from typing import Any
+import numpy as np
+import gymnasium
+import torch
+
+
+@dataclass
+class GoThroughPositionTaskCfg(BaseFormaterCfg):
+    position_tolerance: float = 0.3
+    terminate_early: bool = True
+
+
+class GoThroughPositionFormater(Registerable, BaseFormater):
+    _task_cfg: GoThroughPositionTaskCfg
+
+    def __init__(
+        self,
+        state_preprocessor: BaseStatePreProcessor | None = None,
+        device: str = "cuda",
+        max_steps: int = 500,
+        task_cfg: GoThroughPositionTaskCfg = GoThroughPositionTaskCfg(),
+        num_actions: int = 2,
+        **kwargs,
+    ) -> None:
+        super().__init__(state_preprocessor, device, max_steps, task_cfg)
+
+        # General parameters
+        self.ROS_TYPE = PointStamped
+
+        # Task parameters
+        self._observation_space = gymnasium.spaces.Box(-np.inf, np.inf, (6 + num_actions,))
+
+        self._task_data = torch.zeros((1, 6), device=self._device)
+        self._target_position = torch.zeros((1, 2), device=self._device)
+
+    def build_logs(self):
+        super().build_logs()
+        self._logs["distance_error"] = torch.zeros((1, 1), device=self._device)
+        self._logs["position_heading_error"] = torch.tensor((1, 1), device=self._device)
+        self._logs["target_position"] = torch.tensor((1, 2), device=self._device)
+        self._logs_specs["distance_error"] = [".m"]
+        self._logs_specs["position_heading_error"] = [".rad"]
+        self._logs_specs["target_position"] = [".x.m", ".y.m"]
+
+    def update_logs(self):
+        self._logs["distance_error"] = self.dist
+        self._logs["position_heading_error"] = self.target_heading_error
+        self._logs["target_position"] = self._target_position
+
+    def check_task_completion(self) -> None:
+        """Check if the task has been completed."""
+
+        if self._task_cfg.terminate_early:
+            cart_dist_bool = self.dist < self._task_cfg.position_tolerance
+        else:
+            cart_dist_bool = False
+        time_bool = self._step >= self._max_steps
+
+        self._task_completed = cart_dist_bool or time_bool
+
+    def format_observation(self, actions: torch.Tensor | None = None) -> None:
+        super().format_observation(actions)
+
+        self.dist = torch.linalg.norm(
+            self._target_position - self._state_preprocessor.position[:, :2], dim=1, keepdim=True
+        )
+        target_heading_w = torch.atan2(
+            self._target_position[:, 1] - self._state_preprocessor.position[:, 1],
+            self._target_position[:, 0] - self._state_preprocessor.position[:, 0],
+        )
+
+        self.target_heading_error = torch.atan2(
+            torch.sin(target_heading_w - self._state_preprocessor.heading),
+            torch.cos(target_heading_w - self._state_preprocessor.heading),
+        )
+
+        self._task_data[:, 0:2] = self._state_preprocessor.linear_velocities_body[:, :2]
+        self._task_data[:, 2] = self._state_preprocessor.angular_velocities_body[:, -1]
+        self._task_data[:, 3] = torch.cos(self.target_heading_error)
+        self._task_data[:, 4] = torch.sin(self.target_heading_error)
+        self._task_data[0, 5] = self.dist
+        self._observation = torch.cat((actions, self._task_data), dim=1)
+        self.check_task_completion()
+
+    def update_goal_ROS(self, position: PointStamped | None = None, **kwargs):
+        """Update the goal position using the ROS message.
+        When a goal is received, the task is live and the number of steps is reset.
+
+        Args:
+            position (PointStamped): The goal position."""
+
+        if position is not None:
+            print("Received new goal")
+            print("Going to position: ", position.point.x, position.point.y)
+
+            self._step += 1
+            self._target_position[0, 0] = position.point.x
+            self._target_position[0, 1] = position.point.y
+            # A goal has been received the task is live
+            self._task_is_live = True
+            # Reset the number of steps to 0 when a new goal is received
+            self._step = 0
+
+    def reset(self):
+        super().reset()
+
+        self._target_position.fill_(0)
+        self._task_data.fill_(0)
