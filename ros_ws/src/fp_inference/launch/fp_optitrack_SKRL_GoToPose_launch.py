cc2b73cbf7b25a48affbf2f3f8fd1c05471196e6
AntoineRichard <antoine.richard@gatech.edu>
2025-02-24 14:57:15 +0100

    Base launches

--- ros_ws/src/fp_inference/launch/fp_optitrack_SKRL_GoToPose_launch.py
+++ ros_ws/src/fp_inference/launch/fp_optitrack_SKRL_GoToPose_launch.py
@@ -13,24 +13,23 @@ def generate_launch_description():
                 emulate_tty=True,
                 parameters=[
                     {
-                        "task_name": "GoToPosition",
-                        "state_preprocessor_name": "Odometry",
-                        "robot_interface_name": "VirtualFloatingPlatform",
+                        "task_name": "GoToPose",
+                        "state_preprocessor_name": "Optitrack",
+                        "robot_interface_name": "FloatingPlatform",
                         "inference_runner_name": "SKRL",
                         "enable_logging": True,
                         "device": "cuda:0",
-                        "max_steps": 100,
-                        "dt": 1 / 15.0,
-                        # "nn_log_dir": "/RANS_DeployToRobot/models/skrl/Single/2025-02-17_09-49-57_ppo_torch_FloatingPlatform-GoToPosition",
-                        "nn_log_dir": "/RANS_DeployToRobot/models/skrl/Single/2025-02-17_10-55-30_ppo-discrete_torch_FloatingPlatform-GoToPosition",
+                        "max_steps": 200,
+                        "dt": 1 / 5.0,
+                        "nn_log_dir": "/RANS_DeployToRobot/models/skrl/Single/2025-02-21_17-06-49_ppo-discrete_torch_FloatingPlatform-GoToPosition",
                         "nn_checkpoint_path": "None",
                         "terminate_on_completion": True,
                         "logs_save_path": "/RANS_DeployToRobot/ros_experiments_logs",
                     }
                 ],
                 remappings=[
-                    ("state_preprocessor_input", "/omniFPS/Robots/FloatingPlatform/odom"),
-                    ("robot_interface_commands", "/omniFPS/Robots/FloatingPlatform/thrusters/input"),
+                    ("state_preprocessor_input", "/vrpn_mocap/RigidBody_005/pose"),
+                    ("robot_interface_commands", "spacer_floating_platform/valves/input"),
                 ],
             )
         ]
